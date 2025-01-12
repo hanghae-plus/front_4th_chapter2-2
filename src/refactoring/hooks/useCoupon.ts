67422a15ae53d4d9e3059e1770fa4c7066cc2e48
JunmanChoi <junman9595@gmail.com>
2025-01-13 00:44:19 +0900

    feat: useCoupon 훅 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,11 @@
-import { useState } from 'react';
-import { Coupon } from '../../types';
+import { useCallback, useState } from 'react';
+import { Coupon } from '../../types.ts';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  const addCoupon = useCallback((coupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, coupon]);
+  }, []);
+  return { coupons, addCoupon };
 };
