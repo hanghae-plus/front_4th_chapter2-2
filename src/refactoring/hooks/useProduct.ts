ddb6ee353a18daf5aefee5b047fd942773f36ea9
suzy <susie0529@naver.com>
2025-01-12 19:12:15 +0900

    feat: useProducts - 상품 목록 나타내기

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,17 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (updateProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === updateProduct.id ? updateProduct : p))
+    );
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
