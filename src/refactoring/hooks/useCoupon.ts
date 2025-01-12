2be6772a8bfd66868e3cc8f62180b9ff7651d794
KimBoYoung <qhdud20@naver.com>
2025-01-12 22:58:25 +0900

    ♻ refactor: useCoupons 분리

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,14 @@ import { Coupon } from "../../types.ts";
 import { useState } from "react";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [selectedCoupon, setSelectedCoupon] = useState<Coupon[]>(initialCoupons);
+
+  const applyCoupon = (coupon: Coupon) => {
+    setSelectedCoupon((prevCoupons) => [...prevCoupons, coupon]);
+  };
+
+  return { 
+    coupons: selectedCoupon, 
+    addCoupon: applyCoupon 
+  };
 };
