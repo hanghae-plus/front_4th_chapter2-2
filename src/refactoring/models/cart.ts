930bf27944a7e4ebfd8942ed06ab2c63d92e2627
9yurilee <9yurilee@gmail.com>
2025-01-12 22:36:14 +0900

    refactor: updateCartItemQuantity 함수 작성

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -57,5 +57,14 @@ export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | nu
 // 재고 한도를 초과해서는 안 됩니다
 // useCart에서 참조
 export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-	return [];
+	return cart
+		.map((item) => {
+			if (item.product.id === productId) {
+				const maxQuantity = item.product.stock;
+				const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+				return updatedQuantity > 0 ? { ...item, quantity: updatedQuantity } : null;
+			}
+			return item;
+		})
+		.filter((item): item is CartItem => item !== null);
 };
