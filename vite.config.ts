6fb86b9fae64197f10ad3f78307bc37f5bd2df87
Dana <do.silver2@gmail.com>
2025-01-12 20:43:59 +0900

    :wrench: chore: REACT_APP_MODE 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    define: {
+      "process.env.REACT_APP_MODE": JSON.stringify(
+        process.env.REACT_APP_MODE || "DEFAULT",
+      ),
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
-  })
-)
+  }),
+);
