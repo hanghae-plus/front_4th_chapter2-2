9b911cd2f09347839d715046a1dafa6f9082ee31
zenna <97446606+zenna9@users.noreply.github.com>
2025-01-12 23:24:14 +0900

    :heavy_plus_sign: Chore: eslint 수정

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,25 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest", // 최신 ECMAScript 문법
+        sourceType: "module", // ES 모듈 사용
+        ecmaFeatures: {
+          jsx: true, // JSX 사용
+        },
+      },
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
+      "react/react-in-jsx-scope": "off", // React를 import하지 않아도 JSX 허용
       "no-unused-vars": ["warn"],
     },
   },
