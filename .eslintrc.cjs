b964345a5444cb03376f5f52fe82ed340779ac34
JunmanChoi <junman9595@gmail.com>
2025-01-12 13:55:13 +0900

    chore: eslint 설정 - prettier, airbnb 설정 완료

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,40 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.app.json',
+  },
+  plugins: [
+    'import',
+    'react-refresh',
+    '@typescript-eslint',
+    'react',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/prefer-default-export': 'off',
   },
-}
+};
