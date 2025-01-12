dea6e38e0fe4928db76689c401fcb67f132222ca
Bae seong-kyu <baesense@naver.com>
2025-01-12 23:17:32 +0900

    Refactor : updateCartItemQuantity로 분리

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -24,5 +24,18 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  return [];
+  const result = cart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0
+          ? { ...item, quantity: updatedQuantity }
+          : null;
+      }
+      return item;
+    })
+    .filter((item): item is CartItem => item !== null);
+
+  return result;
 };
