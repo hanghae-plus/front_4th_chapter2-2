0f6883e4bfd87d342c5205d756c2fac9ff0d4f15
LESANF <nagong1000@naver.com>
2025-01-12 18:46:14 +0900

    feat: useProduct 작성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,24 +1,6 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  const [products, setProducts] = useState<Product[]>(initialProducts);
-
-  const updateProduct = (newProduct: Product) => {
-    setProducts((prevProducts) =>
-      prevProducts.map((prevProduct) =>
-        prevProduct.id === newProduct.id ? newProduct : prevProduct
-      )
-    );
-  };
-
-  const addProduct = (newProduct: Product) => {
-    setProducts((prevProducts) => [...prevProducts, newProduct]);
-  };
-
-  return {
-    products,
-    updateProduct,
-    addProduct,
-  };
+  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
 };
