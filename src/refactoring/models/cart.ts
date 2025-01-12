5ab3686631f783c6697a97af167eb7a8bb9dabdf
WonYoung <wy.heo@teamsparta.co>
2025-01-12 17:17:18 +0900

    feat: cart 계산 함수 추가

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,21 +1,58 @@
 import { CartItem, Coupon } from "../../types";
 
+const calculateOriginalTotal = (item: CartItem) => {
+  return item.product.price * item.quantity;
+};
+
+const calculateCouponDiscount = (totalPrice: number, coupon: Coupon | null) => {
+  if (!coupon) return totalPrice;
+
+  if (coupon.discountType === "amount") {
+    return Math.max(0, totalPrice - coupon.discountValue);
+  } else {
+    return totalPrice * (1 - coupon.discountValue / 100);
+  }
+};
+
+/**
+ * 아이템의 총 가격을 계산하는 함수
+ * product의 discounts 중 가장 큰 할인을 적용한다.
+ */
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const maxDiscount = getMaxApplicableDiscount(item);
+  return item.product.price * item.quantity * (1 - maxDiscount);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  const maxDiscount = item.product.discounts.reduce((maxDiscount, d) => {
+    return item.quantity >= d.quantity && d.rate > maxDiscount
+      ? d.rate
+      : maxDiscount;
+  }, 0);
+  return maxDiscount;
 };
 
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+  const totalBeforeDiscount = cart.reduce((total, item) => {
+    return total + calculateOriginalTotal(item);
+  }, 0);
+  const totalAfterProductDiscount = cart.reduce((total, item) => {
+    return total + calculateItemTotal(item);
+  }, 0);
+  const totalAfterDiscount = calculateCouponDiscount(
+    totalAfterProductDiscount,
+    selectedCoupon
+  );
+
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
