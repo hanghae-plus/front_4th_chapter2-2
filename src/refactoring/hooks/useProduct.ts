bea0d6ffaa8ae33306fc301afef2ca3621f5b25f
YeongseoYoon <l112xpla@gmail.com>
2025-01-12 17:21:59 +0900

    feat: useProducts 훅 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,15 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prev) => prev.map((product) => (product.id === updatedProduct.id ? updatedProduct : product)));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prev) => [...prev, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
