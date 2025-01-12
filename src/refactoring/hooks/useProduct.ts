38ef1fd1b7b8a5691982b94ebcdcd510be22742c
9yurilee <9yurilee@gmail.com>
2025-01-12 19:40:52 +0900

    refactor: useProducts hook 작성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,31 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useState } from "react";
+import { Coupon, Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+	const initialCoupons: Coupon[] = [
+		{
+			name: "5000원 할인 쿠폰",
+			code: "AMOUNT5000",
+			discountType: "amount",
+			discountValue: 5000,
+		},
+		{
+			name: "10% 할인 쿠폰",
+			code: "PERCENT10",
+			discountType: "percentage",
+			discountValue: 10,
+		},
+	];
+
+	const [products, setProducts] = useState<Product[]>(initialProducts);
+
+	// 새로운 상품 추가 가능
+	const updateProduct = (updatedProduct: Product) => {
+		setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
+	};
+
+	const addProduct = (newProduct: Product) => {
+		setProducts((prevProducts) => [...prevProducts, newProduct]);
+	};
+	return { products, updateProduct, addProduct };
 };
