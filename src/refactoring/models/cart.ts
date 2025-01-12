b98fef8e9e251f26aa31c2b9e78db731edc4c14c
9yurilee <9yurilee@gmail.com>
2025-01-12 22:02:17 +0900

    refactor: calculateItemTotal 함수 작성

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,13 +1,20 @@
 import { CartItem, Coupon } from "../../types";
 
+// 할인 없이 총액을 계산
 export const calculateItemTotal = (item: CartItem) => {
-	return 0;
+	// 할인율  적용
+	const discountRate = item.product.discounts.filter((prod) => prod.quantity <= item.quantity).sort((a, b) => b.rate - a.rate)[0]?.rate ?? 0;
+
+	return item.quantity * item.product.price * (1 - discountRate);
 };
 
+// 적용 가능한 가장 높은 할인율
 export const getMaxApplicableDiscount = (item: CartItem) => {
 	return 0;
 };
 
+// 쿠폰 없이 총액 계산
+// useCart에서 참조
 export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
 	return {
 		totalBeforeDiscount: 0,
@@ -16,6 +23,10 @@ export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | nu
 	};
 };
 
+// 수량을 올바르게 업데이트해야
+// 수량이 0으로 설정된 경우 항목을 제거해야
+// 재고 한도를 초과해서는 안 됩니다
+// useCart에서 참조
 export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
 	return [];
 };
