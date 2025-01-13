85c5813ae96e13e4eec987d40eeb3caf49650097
uuyeong <proyeongs@gmail.com>
2025-01-13 22:34:54 +0900

    feat: cart 1차 정리

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,21 +1,56 @@
-import { CartItem, Coupon } from "../../types";
+import { CartItem, Coupon, Product } from '../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const noDiscountTotal = getBeforeDiscount(item);
+  const totalDiscount = noDiscountTotal * getMaxApplicableDiscount(item);
+
+  return noDiscountTotal - totalDiscount;
 };
 
+export const getBeforeDiscount = (item: CartItem) => {
+  return item.product.price * item.quantity;
+};
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  let maxDiscountRate = item.product.discounts
+    .filter(({ quantity }) => quantity <= item.quantity)
+    .reduce((max, discount) => Math.max(max, discount.rate), 0);
+
+  return maxDiscountRate;
+};
+
+const calculateCouponDiscount = (
+  price: number,
+  selectedCoupon: Coupon | null
+) => {
+  if (!selectedCoupon) return 0;
+
+  if (selectedCoupon.discountType === 'percentage')
+    return price * selectedCoupon.discountValue * 0.01;
+
+  return selectedCoupon.discountValue;
 };
 
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
+  const totalBeforeDiscount = cart.reduce(
+    (total, item) => (total += getBeforeDiscount(item)),
+    0
+  );
+  const applyDiscount = cart.reduce(
+    (total, item) => (total += calculateItemTotal(item)),
+    0
+  );
+  const couponDiscount = calculateCouponDiscount(applyDiscount, selectedCoupon);
+
+  const totalDiscount = totalBeforeDiscount - applyDiscount + couponDiscount;
+  const totalAfterDiscount = totalBeforeDiscount - totalDiscount;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
   };
 };
 
@@ -24,5 +59,23 @@ export const updateCartItemQuantity = (
   productId: string,
   newQuantity: number
 ): CartItem[] => {
-  return [];
+  const newCarts = cart
+    .map((item) => {
+      if (item.product.id === productId) {
+        const maxQuantity = item.product.stock;
+        const updatedQuantity = Math.max(0, Math.min(newQuantity, maxQuantity));
+        return updatedQuantity > 0
+          ? { ...item, quantity: updatedQuantity }
+          : null;
+      }
+      return item;
+    })
+    .filter((item) => item !== null);
+
+  return newCarts;
+};
+
+export const getRemainingStock = (cart: CartItem[], product: Product) => {
+  const cartItem = cart.find((item) => item.product.id === product.id);
+  return product.stock - (cartItem?.quantity || 0);
 };
