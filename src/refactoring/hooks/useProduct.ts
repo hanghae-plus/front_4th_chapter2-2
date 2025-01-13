64905a2dccdf44aa90b059e72f4e00e7c1633914
jungeun0712 <kje970712@naver.com>
2025-01-13 14:42:53 +0900

    feat: useProduct custom hook 로직 추가
    1. 상품 정보 업데이트(계산 및 액션 분리)\n2. 새 상품 추가 로직(계산 및 액션 분리)

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,24 @@
 import { useState } from 'react';
 import { Product } from '../../types.ts';
 
+// 순수함수 분리??
+const updateProductList = (prevProducts: Product[], updatedProduct: Product): Product[] => {
+  return prevProducts.map(prevProduct => (prevProduct.id === updatedProduct.id ? updatedProduct : prevProduct));
+};
+const addProductList = (prevProducts: Product[], newProduct: Product): Product[] => {
+  return [...prevProducts, newProduct];
+};
+
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const handleProductUpdate = (updatedProduct: Product) => {
+    setProducts(prevProducts => updateProductList(prevProducts, updatedProduct));
+  };
+
+  const handleProductAdd = (newProduct: Product) => {
+    setProducts(prevProducts => addProductList(prevProducts, newProduct));
+  };
+
+  return { products: products, updateProduct: handleProductUpdate, addProduct: handleProductAdd };
 };
