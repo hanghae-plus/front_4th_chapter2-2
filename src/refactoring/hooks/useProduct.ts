5332ce2c833172061ec064117d592350265f2ac1
Changyeop Song <waitwait0301@naver.com>
2025-01-13 23:19:12 +0900

    feat: useProduct 로직 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -3,5 +3,17 @@ import { useState } from 'react';
 import type { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (newProduct: Product) => {
+    const updateProducts = products.map((product) => (product.id === newProduct.id ? newProduct : product));
+
+    setProducts(updateProducts);
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((prev) => [...prev, product]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
