5e8d1de82222450d9cff5985c0f66cdf37bd473a
nogy21 <nogy21@gmail.com>
2025-01-13 22:27:10 +0900

    ✨ 쿠폰 목록 관리 기능을 추가하고, 새로운 쿠폰을 추가하는 순수 함수 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import { Coupon } from '../../types';
 
-export const useCoupons = (initialCoupons: Coupon[]) => ({
-  coupons: [],
-  addCoupon: () => undefined,
-});
+/**
+ * 쿠폰 목록을 관리하는 커스텀 훅
+ */
+export const useCoupons = (initialCoupons: Coupon[]) => {
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => setCoupons(addCouponInList(coupons, newCoupon));
+
+  return {
+    coupons,
+    addCoupon,
+  };
+};
+
+/**
+ * 새로운 쿠폰을 쿠폰 목록에 추가하는 순수 함수
+ */
+const addCouponInList = (coupons: Coupon[], newCoupon: Coupon) => [...coupons, newCoupon];
