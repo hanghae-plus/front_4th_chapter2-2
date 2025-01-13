47755909a5aba764538a7427b5710fe8c36ee458
yireum <lastepisode@naver.com>
2025-01-13 22:56:11 +0900

    ✨ feat: useProduct, useCoupon 작성

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,13 @@
-import { Coupon } from "../../types.ts";
 import { useState } from "react";
+import { Coupon } from "@/types";
 
+// 쿠폰 목록 관리
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons([...coupons, newCoupon]);
+  };
+
+  return { coupons, addCoupon };
 };
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,20 @@
 import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { Product } from '@/types';
 
+// 상품 목록 관리
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(products.map(product => 
+      // id 같은 상품만 업데이트
+      product.id === updatedProduct.id ? updatedProduct : product
+    ));
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts([...products, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
