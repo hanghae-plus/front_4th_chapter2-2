775a89196a8bc29fdcc8ae1306ae04ef39c24f28
김재환 <fodore12@gmail.com>
2025-01-13 21:14:47 +0900

    feat: useProducts 작성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,24 @@
 import { useState } from 'react';
 import { Product } from '../../types.ts';
 
-export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+export const useProducts = (initialProductList: Product[]) => {
+  const [productList, setProductList] = useState<Product[]>(initialProductList);
+
+  const handleUpdateProductList = (updatedProduct: Product) => {
+    setProductList((prevProductList) =>
+      prevProductList.map((product) =>
+        product.id === updatedProduct.id ? updatedProduct : product,
+      ),
+    );
+  };
+
+  const handleAddProduct = (newProduct: Product) => {
+    setProductList((prevProductList) => [...prevProductList, newProduct]);
+  };
+
+  return {
+    products: productList,
+    updateProduct: handleUpdateProductList,
+    addProduct: handleAddProduct,
+  };
 };
