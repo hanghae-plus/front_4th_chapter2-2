b5496c7f11ee56544b070bbe1b547382257629e2
hyunjin <guswns1419@naver.com>
2025-01-14 02:19:14 +0900

    feat : PageLayout 분리

--- src/refactoring/App.tsx
+++ src/refactoring/App.tsx
@@ -1,42 +1,42 @@
 import { useState } from "react";
-import { CartPage } from "./components/CartPage.tsx";
-import { AdminPage } from "./components/AdminPage.tsx";
+import { CartPage } from "./pages/CartPage";
+import { AdminPage } from "./pages/AdminPage";
 import { useCoupons, useProducts } from "./hooks";
-import { initialCoupons, initialProducts } from "./constants/initialValues.ts";
+import { initialCoupons, initialProducts } from "./constants/initialValues";
+import PageLayout from "./components/layout/PageLayout";
 
 const App = () => {
   const { products, updateProduct, addProduct } = useProducts(initialProducts);
   const { coupons, addCoupon } = useCoupons(initialCoupons);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isViewingAdminPage, setIsViewingAdminPage] = useState(false);
+
+  const toggleAdminPage = () => {
+    setIsViewingAdminPage((prevState) => !prevState);
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <nav className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
-          <button
-            onClick={() => setIsAdmin(!isAdmin)}
-            className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100"
-          >
-            {isAdmin ? "장바구니 페이지로" : "관리자 페이지로"}
-          </button>
-        </div>
-      </nav>
-
-      <main className="container mx-auto mt-6">
-        {isAdmin ? (
-          <AdminPage
-            products={products}
-            coupons={coupons}
-            onProductUpdate={updateProduct}
-            onProductAdd={addProduct}
-            onCouponAdd={addCoupon}
-          />
-        ) : (
-          <CartPage products={products} coupons={coupons} />
-        )}
-      </main>
-    </div>
+    <PageLayout
+      actions={
+        <button
+          onClick={toggleAdminPage}
+          className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-blue-100"
+        >
+          {isViewingAdminPage ? "장바구니 페이지로" : "관리자 페이지로"}
+        </button>
+      }
+    >
+      {isViewingAdminPage ? (
+        <AdminPage
+          products={products}
+          coupons={coupons}
+          onProductUpdate={updateProduct}
+          onProductAdd={addProduct}
+          onCouponAdd={addCoupon}
+        />
+      ) : (
+        <CartPage products={products} coupons={coupons} />
+      )}
+    </PageLayout>
   );
 };
 
---
+++ src/refactoring/components/layout/PageLayout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface PageLayoutProps {
+  children: ReactNode;
+  actions: JSX.Element;
+}
+
+const PageLayout = ({ children, actions }: PageLayoutProps) => (
+  <div className="min-h-screen bg-gray-100">
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">쇼핑몰 관리 시스템</h1>
+        {actions}
+      </div>
+    </nav>
+
+    <main className="container mx-auto mt-6">{children}</main>
+  </div>
+);
+
+export default PageLayout;
