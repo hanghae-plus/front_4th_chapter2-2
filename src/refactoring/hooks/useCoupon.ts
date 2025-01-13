b0e401dec5782458f8b9d631e038f3f2f3511b49
bongjoonkim <bongjoonkim96@gmail.com>
2025-01-13 21:50:52 +0900

    기본 과제 수행 완료

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,19 @@ import { Coupon } from "../../types.ts";
 import { useState } from "react";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+  const addCoupon = (newCoupon) => {
+    setCoupons(prev => {
+      return [
+        ...prev,
+        newCoupon
+      ]
+    })
+  }
+  
+  return {
+    coupons,
+    addCoupon
+  };
 };
