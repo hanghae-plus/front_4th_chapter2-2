70eb7f24f9bfab824f0b83f919b21cfa20eb24e1
nogy21 <nogy21@gmail.com>
2025-01-13 20:56:51 +0900

    🔧 tsconfig 및 vite.config 수정: 팀 컨벤션 적용(TypeScript 설정 개선 및 파일 확장자 지원 추가)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'], // 파일 확장자 지원
+    },
   }),
   defineTestConfig({
     test: {
