b3c944c29f8b77e93e784a7906928140e94bb4b6
nogy21 <nogy21@gmail.com>
2025-01-13 22:24:29 +0900

    ♻️: useProducts 훅의 제품 업데이트 및 추가 로직을 순수 함수로 리팩토링

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -8,11 +8,11 @@ export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
   const updateProduct = (product: Product) => {
-    setProducts((prevProducts) => prevProducts.map((p) => (p.id === product.id ? product : p)));
+    setProducts((prevProducts) => updateProductInList(prevProducts, product));
   };
 
   const addProduct = (product: Product) => {
-    setProducts((prevProducts) => [...prevProducts, product]);
+    setProducts((prevProducts) => addProductToList(prevProducts, product));
   };
 
   return {
@@ -21,3 +21,17 @@ export const useProducts = (initialProducts: Product[]) => {
     addProduct,
   };
 };
+
+/**
+ * 기존 제품 목록에서 제품을 업데이트하는 순수 함수
+ */
+const updateProductInList = (products: Product[], updatedProduct: Product): Product[] =>
+  products.map((product) => (product.id === updatedProduct.id ? updatedProduct : product));
+
+/**
+ * 새로운 제품을 목록에 추가하는 순수 함수
+ */
+const addProductToList = (products: Product[], newProduct: Product): Product[] => [
+  ...products,
+  newProduct,
+];
