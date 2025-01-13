163d186d71eb9c638afed3c3b14bb2c37af1fc34
jungeun <jungeun.lee@teamsparta.co>
2025-01-13 15:14:01 +0900

    feat: 초기 셋팅 1) 코드 읽고 덩어리 잡기 2) 데이터 분리 3) 프리티어 적용

--- src/refactoring/hooks/useCart.ts
+++ src/refactoring/hooks/useCart.ts
@@ -1,7 +1,6 @@
 // useCart.ts
-import { useState } from "react";
-import { CartItem, Coupon, Product } from "../../types";
-import { calculateCartTotal, updateCartItemQuantity } from "../models/cart";
+import { useState } from 'react';
+import { CartItem, Coupon, Product } from '../../types';
 
 export const useCart = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
