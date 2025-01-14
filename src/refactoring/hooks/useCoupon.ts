b4e43d1b068b1e8a84ae8ac3de2d3e780eb008ff
Blue-Kite <bluelemon112@gmail.com>
2025-01-14 13:01:59 +0900

    feat: useCoupon, useProduct 훅 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+import { Coupon } from '../../types';
+import { initialCoupons } from '../models/initialData';
+
 export const useCoupons = () => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+
+  return { coupons, addCoupon };
 };
