f64cc89e4ad2a81ebb6827af6a4d06abb48dc275
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-01-15 00:42:11 +0900

    fix: 상품의 엔티티 상태를 관리하는 useProduct.ts 업데이트

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Product } from '../../types.ts';
 
+// useProducts : 상품의 엔티티 상태(init/update/add)를 담당하는 커스텀 훅
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  useEffect(() => {
+    // 예외처리 및 상태 init
+    if (initialProducts) setProducts(initialProducts);
+  }, [initialProducts]);
+
+  // 상품 업데이트
+  const updateProduct = (product: Product) => {
+    const findIndex = products.findIndex(prod => prod.id === product.id);
+    const copyProducts = [...products];
+    copyProducts[findIndex] = product;
+    setProducts(copyProducts);
+  }
+
+  // 상품 추가
+  const addProduct = (product: Product) => setProducts([...products, product])
+
+  return {
+    products: products, 
+    updateProduct: updateProduct,
+    addProduct: addProduct
+  };
 };
