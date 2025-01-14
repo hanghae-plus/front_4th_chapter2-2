9ae2abcd47a0bd5abbe16f79c49d68e687348a36
pitang <wpeffect@gmail.com>
2025-01-15 01:24:58 +0900

    ✨ useProduct 훅 구현
    - `초기 제품 목록을 상태로 관리하는 `useProducts` 추가
    - `updateProduct`:특정 제품 정보를 업데이트
    - `addProduct`:새로운 제품 추가`

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,21 @@
 import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { Product } from '../../types';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  //특정 제품 정보를 업데이트
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)),
+    );
+  };
+
+  //새로운 제품 추가
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
+  // 현재 제품 상태 및 변경 함수 반환
+  return { products, updateProduct, addProduct };
 };
