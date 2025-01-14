ed956c372f70cef65e3c10552df278d6c57d4708
effozen <ashgrayblue0@gmail.com>
2025-01-15 01:01:40 +0900

    feat: useProduct 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,19 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const handleProductUpdate = (updatedProduct: Product) => {
+    setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
+  };
+
+  const handleProductAdd = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct: handleProductUpdate,
+    addProduct: handleProductAdd,
   };
 };
