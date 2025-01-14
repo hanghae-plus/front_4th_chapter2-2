7d364ba7dbd33c722556e2fd5dd4f4eaa0e230fd
KimYoungWoo <rladuddn3924@gmail.com>
2025-01-14 21:49:04 +0900

    ♻️ AdminPage 컴포넌트 리팩토링
    - ProductDiscount 컴포넌트 분리

--- src/refactoring/components/AdminPage.tsx
+++ src/refactoring/components/AdminPage.tsx
@@ -6,6 +6,7 @@ import useNewProduct from '../hooks/useNewProduct.ts';
 import EditProductForm from './EditProductForm.tsx';
 import ProductForm from './ProductForm.tsx';
 import ManageCoupon from './ManageCoupon.tsx';
+import ProductDiscount from './ProductDiscount.tsx';
 
 interface Props {
   products: Product[];
@@ -71,22 +72,7 @@ export const AdminPage = ({
                         {...adminProps}
                       />
                     ) : (
-                      <div>
-                        {product.discounts.map((discount, index) => (
-                          <div key={index} className='mb-2'>
-                            <span>
-                              {discount.quantity}개 이상 구매 시 {discount.rate * 100}% 할인
-                            </span>
-                          </div>
-                        ))}
-                        <button
-                          data-testid='modify-button'
-                          onClick={() => handleEditProduct(product)}
-                          className='bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 mt-2'
-                        >
-                          수정
-                        </button>
-                      </div>
+                      <ProductDiscount product={product} handleEditProduct={handleEditProduct} />
                     )}
                   </div>
                 )}
