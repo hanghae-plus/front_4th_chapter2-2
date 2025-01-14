f3ac6d7cf3d318c0b0f4f4e5bf6090d10549c2f8
Dayglow0926 <dayglow0926@gmail.com>
2025-01-14 22:18:23 +0900

    feat: util 함수 추가 - getRemainingStock - getAppliedDiscount - getMaxDiscount

---
+++ src/refactoring/utils/cart.ts
@@ -0,0 +1,24 @@
+import { CartItem, Product } from '../../types';
+
+export const getRemainingStock = (product: Product, cartList: CartItem[]) => {
+  const cartItem = cartList.find((item) => item.product.id === product.id);
+  return product.stock - (cartItem?.quantity || 0);
+};
+
+export const getAppliedDiscount = (item: CartItem) => {
+  const { discounts } = item.product;
+  const { quantity } = item;
+  let appliedDiscount = 0;
+  for (const discount of discounts) {
+    if (quantity >= discount.quantity) {
+      appliedDiscount = Math.max(appliedDiscount, discount.rate);
+    }
+  }
+  return appliedDiscount;
+};
+
+export const getMaxDiscount = (
+  discounts: { quantity: number; rate: number }[]
+) => {
+  return discounts.reduce((max, discount) => Math.max(max, discount.rate), 0);
+};
