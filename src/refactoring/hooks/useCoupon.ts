1381b20c83ea03b5142128e8025ecf20fd2e5a81
pitang <wpeffect@gmail.com>
2025-01-15 01:25:23 +0900

    ✨ useCoupons 훅 구현
    - `초기 쿠폰 목록을 상태로 관리하는 `useCoupons` 추가
    - `addCoupon`: 새로운 쿠폰을 추가`

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,14 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { Coupon } from '../../types';
+import { useState } from 'react';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  // 새로운 쿠폰 추가
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
+
+  // 현재 쿠폰 상태와 추가 함수 반환
+  return { coupons, addCoupon };
 };
