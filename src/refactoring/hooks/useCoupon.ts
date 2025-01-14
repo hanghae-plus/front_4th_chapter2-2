29cb9656bba535e0c850060398354cdfb99d07f8
9yurilee <9yurilee@gmail.com>
2025-01-14 10:06:03 +0900

    style: prettier 포맷팅 적용

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,11 +2,11 @@ import { Coupon } from "../../types.ts";
 import { useState } from "react";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-	const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
 
-	const addCoupon = (newCoupon: Coupon) => {
-		setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
-	};
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
+  };
 
-	return { coupons, addCoupon };
+  return { coupons, addCoupon };
 };
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,31 +1,18 @@
 import { useState } from "react";
-import { Coupon, Product } from "../../types.ts";
+import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-	const initialCoupons: Coupon[] = [
-		{
-			name: "5000원 할인 쿠폰",
-			code: "AMOUNT5000",
-			discountType: "amount",
-			discountValue: 5000,
-		},
-		{
-			name: "10% 할인 쿠폰",
-			code: "PERCENT10",
-			discountType: "percentage",
-			discountValue: 10,
-		},
-	];
+  const [products, setProducts] = useState<Product[]>(initialProducts);
 
-	const [products, setProducts] = useState<Product[]>(initialProducts);
+  // 새로운 상품 추가 가능
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p))
+    );
+  };
 
-	// 새로운 상품 추가 가능
-	const updateProduct = (updatedProduct: Product) => {
-		setProducts((prevProducts) => prevProducts.map((p) => (p.id === updatedProduct.id ? updatedProduct : p)));
-	};
-
-	const addProduct = (newProduct: Product) => {
-		setProducts((prevProducts) => [...prevProducts, newProduct]);
-	};
-	return { products, updateProduct, addProduct };
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+  return { products, updateProduct, addProduct };
 };
