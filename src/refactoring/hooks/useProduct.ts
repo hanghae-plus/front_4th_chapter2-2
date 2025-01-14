0b8df8fec4621bfc03d9a371fee9d723dcbc93e7
nopinokio <nopinokio90@gmail.com>
2025-01-14 15:45:22 +0900

    refactor: 어드민의 updateProduct, addProduct 분리

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -4,5 +4,16 @@ import { Product } from '../../types.ts';
 export const useProducts = (initialProducts: Product[]) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
 
-  return { products };
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(prevProducts =>
+      prevProducts.map(p => p.id === updatedProduct.id ? updatedProduct : p)
+    );
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts(prevProducts => [...prevProducts, newProduct]);
+  };
+
+
+  return { products, updateProduct, addProduct };
 };
