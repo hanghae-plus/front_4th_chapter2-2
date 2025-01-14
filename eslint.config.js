17a284fdb03d6d36b8e38eb81e836cfff731e14d
Geunbaek <rmsqor103@naver.com>
2025-01-14 18:15:26 +0900

    chore: eslint & prettier 적용

---
+++ eslint.config.js
@@ -0,0 +1,77 @@
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import a11y from 'eslint-plugin-jsx-a11y';
+import vitest from 'eslint-plugin-vitest';
+import prettier from 'eslint-plugin-prettier';
+import airbnbTs from 'eslint-config-airbnb-typescript';
+
+export default [
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: [
+          './tsconfig.json',
+          './tsconfig.app.json',
+          './tsconfig.node.json',
+        ],
+        sourceType: 'module',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': a11y,
+      vitest,
+      prettier,
+    },
+    rules: {
+      ...airbnbTs.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off', // React 17+ JSX transform 사용
+      'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'function-declaration',
+          unnamedComponents: 'arrow-function',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'import/prefer-default-export': 'off', // 유틸 함수 내보내기 규칙 맞춤
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/anchor-is-valid': 'off', // Next.js의 <Link> 지원
+    },
+  },
+  {
+    files: ['src/*.test.{ts,tsx}'],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      'vitest/no-disabled-tests': 'warn',
+      'vitest/no-focused-tests': 'error',
+      'vitest/no-identical-title': 'error',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'src/origin'],
+  },
+];
