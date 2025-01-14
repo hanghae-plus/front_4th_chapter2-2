47e805aea2a6542c2380254923e695dcbb664079
KimYoungWoo <rladuddn3924@gmail.com>
2025-01-14 21:37:18 +0900

    ♻️ AdminPage 컴포넌트 리팩토링
    - EditProductForm 컴포넌트 분리

--- src/refactoring/components/AdminPage.tsx
+++ src/refactoring/components/AdminPage.tsx
@@ -3,6 +3,7 @@ import useAdmin from '../hooks/useAdmin.ts';
 import useNewCoupon from '../hooks/useNewCoupon.ts';
 import useProductSet from '../hooks/useProductSet.ts';
 import useNewProduct from '../hooks/useNewProduct.ts';
+import EditProductForm from './EditProductForm.tsx';
 
 interface Props {
   products: Product[];
@@ -19,19 +20,10 @@ export const AdminPage = ({
   onProductAdd,
   onCouponAdd,
 }: Props) => {
-  const {
-    editingProduct,
-    newDiscount,
-    handleChangeRate,
-    handleChangeQuantity,
-    handleEditProduct,
-    handleProductNameUpdate,
-    handlePriceUpdate,
-    handleEditComplete,
-    handleStockUpdate,
-    handleAddDiscount,
-    handleRemoveDiscount,
-  } = useAdmin({ products, onProductUpdate });
+  const { handleEditProduct, editingProduct, ...adminProps } = useAdmin({
+    products,
+    onProductUpdate,
+  });
 
   const {
     newCoupon,
@@ -130,84 +122,11 @@ export const AdminPage = ({
                 {openProductIds.has(product.id) && (
                   <div className='mt-2'>
                     {editingProduct && editingProduct.id === product.id ? (
-                      <div>
-                        <div className='mb-4'>
-                          <label className='block mb-1'>상품명: </label>
-                          <input
-                            type='text'
-                            value={editingProduct.name}
-                            onChange={(e) => handleProductNameUpdate(product.id, e.target.value)}
-                            className='w-full p-2 border rounded'
-                          />
-                        </div>
-                        <div className='mb-4'>
-                          <label className='block mb-1'>가격: </label>
-                          <input
-                            type='number'
-                            value={editingProduct.price}
-                            onChange={(e) =>
-                              handlePriceUpdate(product.id, parseInt(e.target.value))
-                            }
-                            className='w-full p-2 border rounded'
-                          />
-                        </div>
-                        <div className='mb-4'>
-                          <label className='block mb-1'>재고: </label>
-                          <input
-                            type='number'
-                            value={editingProduct.stock}
-                            onChange={(e) =>
-                              handleStockUpdate(product.id, parseInt(e.target.value))
-                            }
-                            className='w-full p-2 border rounded'
-                          />
-                        </div>
-                        {/* 할인 정보 수정 부분 */}
-                        <div>
-                          <h4 className='text-lg font-semibold mb-2'>할인 정보</h4>
-                          {editingProduct.discounts.map((discount, index) => (
-                            <div key={index} className='flex justify-between items-center mb-2'>
-                              <span>
-                                {discount.quantity}개 이상 구매 시 {discount.rate * 100}% 할인
-                              </span>
-                              <button
-                                onClick={() => handleRemoveDiscount(product.id, index)}
-                                className='bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600'
-                              >
-                                삭제
-                              </button>
-                            </div>
-                          ))}
-                          <div className='flex space-x-2'>
-                            <input
-                              type='number'
-                              placeholder='수량'
-                              value={newDiscount.quantity}
-                              onChange={handleChangeQuantity}
-                              className='w-1/3 p-2 border rounded'
-                            />
-                            <input
-                              type='number'
-                              placeholder='할인율 (%)'
-                              value={newDiscount.rate * 100}
-                              onChange={handleChangeRate}
-                              className='w-1/3 p-2 border rounded'
-                            />
-                            <button
-                              onClick={() => handleAddDiscount(product.id)}
-                              className='w-1/3 bg-blue-500 text-white p-2 rounded hover:bg-blue-600'
-                            >
-                              할인 추가
-                            </button>
-                          </div>
-                        </div>
-                        <button
-                          onClick={handleEditComplete}
-                          className='bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600 mt-2'
-                        >
-                          수정 완료
-                        </button>
-                      </div>
+                      <EditProductForm
+                        product={product}
+                        editingProduct={editingProduct}
+                        {...adminProps}
+                      />
                     ) : (
                       <div>
                         {product.discounts.map((discount, index) => (
