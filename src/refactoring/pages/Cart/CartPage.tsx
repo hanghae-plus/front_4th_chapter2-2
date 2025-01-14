5ece37820cb081bd9208c414ba487cdac2dea9ed
jch1223 <ico1828@gmail.com>
2025-01-14 22:39:31 +0900

    🏗️ AppliedCoupon 컴포넌트 분리

--- src/refactoring/pages/Cart/CartPage.tsx
+++ src/refactoring/pages/Cart/CartPage.tsx
@@ -98,27 +98,7 @@ export const CartPage = ({ products, coupons }: Props) => {
             <h2 className="mb-2 text-2xl font-semibold">쿠폰 적용</h2>
 
             <CouponSelect coupons={coupons} onCouponApply={applyCoupon} />
-            <select
-              onChange={e => applyCoupon(coupons[parseInt(e.target.value)])}
-              className="mb-2 w-full rounded border p-2"
-            >
-              <option value="">쿠폰 선택</option>
-              {coupons.map((coupon, index) => (
-                <option key={coupon.code} value={index}>
-                  {coupon.name} -{' '}
-                  {coupon.discountType === 'amount' ? `${coupon.discountValue}원` : `${coupon.discountValue}%`}
-                </option>
-              ))}
-            </select>
-            {selectedCoupon && (
-              <p className="text-green-600">
-                적용된 쿠폰: {selectedCoupon.name}(
-                {selectedCoupon.discountType === 'amount'
-                  ? `${selectedCoupon.discountValue}원`
-                  : `${selectedCoupon.discountValue}%`}{' '}
-                할인)
-              </p>
-            )}
+            {selectedCoupon && <AppliedCoupon appliedCoupon={selectedCoupon} />}
           </div>
 
           <div className="mt-6 rounded bg-white p-4 shadow">
