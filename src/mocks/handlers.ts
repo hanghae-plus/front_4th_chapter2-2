9bfa9f45ef8b55c49aaf9f25ba61dd50ec49d6fe
Dana <do.silver2@gmail.com>
2025-01-15 07:59:04 +0900

    :sparkles: feat: Products msw handler 구현

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,16 @@
-export const handlers = [];
+import { http, HttpResponse } from "msw";
+import { Product } from "../types";
+import mockProducts from "./data/products.json";
+
+const mockStorage: Map<string, object> = new Map();
+mockStorage.set("/products", mockProducts);
+
+export const handlers = [
+  http.get<never, never, Product[]>("/products", () => {
+    return HttpResponse.json(mockStorage.get("/products") as Product[]);
+  }),
+  http.put<never, Product[], Product[]>("/products", async ({ request }) => {
+    mockStorage.set("/products", request.json());
+    return HttpResponse.json(mockStorage.get("/products") as Product[]);
+  }),
+];
