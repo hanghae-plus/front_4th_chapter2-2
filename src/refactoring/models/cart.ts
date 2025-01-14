6e5f71addba8566a127e3fe771b352a3c60ee277
nopinokio <nopinokio90@gmail.com>
2025-01-15 00:22:44 +0900

    feat: calculateItemTotal 수량에 따라 올바른 할인

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,11 +1,19 @@
 import { CartItem, Coupon } from "../../types";
 
-export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+export const calculateItemTotal = (item: CartItem):number => {
+  return (item.product.price * item.quantity * (1 - getMaxApplicableDiscount(item)));
 };
 
-export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+export const getMaxApplicableDiscount = (item: CartItem):number => {
+  const { discounts } = item.product;
+    const { quantity } = item;
+    let appliedDiscount = 0;
+    for (const discount of discounts) {
+      if (quantity >= discount.quantity) {
+        appliedDiscount = Math.max(appliedDiscount, discount.rate);
+      }
+    }
+    return appliedDiscount;
 };
 
 export const calculateCartTotal = (
