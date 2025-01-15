8d94f9bedf7303aa89716b49ad5d3e0705988517
SU_VIN <pyotnqls25@naver.com>
2025-01-16 03:28:37 +0900

    refactor: adminPage widget 분리

--- src/refactoring/app/App.tsx
+++ src/refactoring/app/App.tsx
@@ -1,55 +1,9 @@
 import { useState } from 'react';
 import { CartPage } from '../pages/user/CartPage.tsx';
 import { AdminPage } from '../pages/admin/AdminPage.tsx';
-import { Coupon, Product } from '../shared/types/types.ts';
-import { useCoupons, useProducts } from '../hooks/index.ts';
 import Header from '../pages/shared/Header.tsx';
 
-const initialProducts: Product[] = [
-  {
-    id: 'p1',
-    name: '상품1',
-    price: 10000,
-    stock: 20,
-    discounts: [
-      { quantity: 10, rate: 0.1 },
-      { quantity: 20, rate: 0.2 },
-    ],
-  },
-  {
-    id: 'p2',
-    name: '상품2',
-    price: 20000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.15 }],
-  },
-  {
-    id: 'p3',
-    name: '상품3',
-    price: 30000,
-    stock: 20,
-    discounts: [{ quantity: 10, rate: 0.2 }],
-  },
-];
-
-const initialCoupons: Coupon[] = [
-  {
-    name: '5000원 할인 쿠폰',
-    code: 'AMOUNT5000',
-    discountType: 'amount',
-    discountValue: 5000,
-  },
-  {
-    name: '10% 할인 쿠폰',
-    code: 'PERCENT10',
-    discountType: 'percentage',
-    discountValue: 10,
-  },
-];
-
 const App = () => {
-  const { products, updateProduct, addProduct } = useProducts(initialProducts);
-  const { coupons, addCoupon } = useCoupons(initialCoupons);
   const [isAdmin, setIsAdmin] = useState(false);
 
   const swapRoleHandler = () => {
@@ -59,19 +13,7 @@ const App = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header isAdmin={isAdmin} onSwapRole={swapRoleHandler} />
-      <main className="container mx-auto mt-6">
-        {isAdmin ? (
-          <AdminPage
-            products={products}
-            coupons={coupons}
-            onProductUpdate={updateProduct}
-            onProductAdd={addProduct}
-            onCouponAdd={addCoupon}
-          />
-        ) : (
-          <CartPage />
-        )}
-      </main>
+      <main className="container mx-auto mt-6">{isAdmin ? <AdminPage /> : <CartPage />}</main>
     </div>
   );
 };
