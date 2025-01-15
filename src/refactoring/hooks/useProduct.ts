1469949e772b7844f0cebbc13c14242a73332291
jmlee9707 <jmlee9707@naver.com>
2025-01-16 03:56:48 +0900

    feat : useCart, useCoupon, useProduct hook 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,5 +2,17 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (targetProduct: Product) => {
+    setProducts((prev) =>
+      prev.map((product) => (product.id === targetProduct.id ? targetProduct : product)),
+    );
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prev) => [...prev, newProduct]);
+  };
+
+  return { products, updateProduct, addProduct };
 };
