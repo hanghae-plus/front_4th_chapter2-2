ca7b7f33a9b449e3aaf6b4a5e83457657bcb96f2
Changyeop Song <waitwait0301@naver.com>
2025-01-15 22:04:29 +0900

    refactor: ProductManager 내부에서 ProductItem의 props drilling을 해소하고 UI와 로직을 분리하도록 render props패턴 적용

--- src/refactoring/domains/admin/components/product/ProductManager.tsx
+++ src/refactoring/domains/admin/components/product/ProductManager.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 
+import { ProductEditForm } from './ProductEditForm';
 import { ProductForm } from './ProductForm';
-import { ProductItem } from './ProductItem';
+import { ProductItemContainer } from './ProductItemContainer';
+import { ProductItemDetail } from './ProductItemDetail';
 
 import type { Product } from '../../../../../types';
 
@@ -24,13 +26,54 @@ export const ProductManager = ({ products, onProductUpdate, onProductAdd }: Prod
         {showNewProductForm ? '취소' : '새 상품 추가'}
       </button>
       {showNewProductForm && <ProductForm onProductAdd={onProductAdd} onToggleShowProductForm={toggleProductForm} />}
+      {/* 상품 목록 */}
       <div className="space-y-2">
         {products.map((product, index) => (
           <div key={product.id} data-testid={`product-${index + 1}`} className="bg-white p-4 rounded shadow">
-            <ProductItem product={product} onProductUpdate={onProductUpdate} />
+            <ProductItemContainer
+              render={(openProductIds, showEditForm, toggleProductAccordion, toggleEditForm) => {
+                const isOpenProduct = openProductIds.has(product.id);
+                return (
+                  <>
+                    <ProductItem product={product} toggleProductAccordion={toggleProductAccordion} />
+                    {isOpenProduct && (
+                      <div className="mt-2">
+                        {showEditForm ? (
+                          <ProductEditForm
+                            product={product}
+                            onProductUpdate={onProductUpdate}
+                            onToggleEditForm={toggleEditForm}
+                          />
+                        ) : (
+                          <ProductItemDetail product={product} onToggleEditForm={toggleEditForm} />
+                        )}
+                      </div>
+                    )}
+                  </>
+                );
+              }}
+            />
           </div>
         ))}
       </div>
     </>
+  );
+};
+
+const ProductItem = ({
+  product,
+  toggleProductAccordion,
+}: {
+  product: Product;
+  toggleProductAccordion: (productId: string) => void;
+}) => {
+  return (
+    <button
+      data-testid="toggle-button"
+      onClick={() => toggleProductAccordion(product.id)}
+      className="w-full text-left font-semibold"
+    >
+      {product.name} - {product.price}원 (재고: {product.stock})
+    </button>
   );
 };
