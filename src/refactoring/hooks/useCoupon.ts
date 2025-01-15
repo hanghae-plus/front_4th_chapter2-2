04ffefb524983ded1de369c1e17e39e38ab871ff
haaneul11 <sksmsqodn20@naver.com>
2025-01-15 21:35:24 +0900

    refactor : usecoupon hook 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -1,6 +1,17 @@
-import { Coupon } from "../../types.ts";
-import { useState } from "react";
+import { Coupon } from '../../types.ts';
+import { useState } from 'react';
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  // 쿠폰을 초기화할 수 있다.
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  // 쿠폰을 추가할 수 있다.
+  const addCoupon = (newCoupon: Coupon) => {
+    setCoupons(prevCoupons => [...prevCoupons, newCoupon]);
+  };
+
+  return {
+    coupons,
+    addCoupon,
+  };
 };
