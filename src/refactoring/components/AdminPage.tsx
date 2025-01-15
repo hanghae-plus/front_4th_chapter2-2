7b7379ac453d470820f6590ca51fac76551ab3a2
Dayglow0926 <dayglow0926@gmail.com>
2025-01-16 01:24:55 +0900

    ♻️: 쿠폰 리스트 UI 관심사 분리
    - AdminCouponList 컴포넌트 생성

--- src/refactoring/components/AdminPage.tsx
+++ src/refactoring/components/AdminPage.tsx
@@ -3,6 +3,7 @@ import { AdminProductManagementWrapper } from './admin/AdminProductManagementWra
 import { AdminNewProduct } from './admin/AdminNewProduct.tsx';
 import { AdminProductList } from './admin/AdminProductList.tsx';
 import { AdminNewCoupon } from './admin/AdminNewCoupon.tsx';
+import { AdminCouponList } from './admin/AdminCouponList.tsx';
 
 interface Props {
   productList: Product[];
@@ -31,24 +32,7 @@ export const AdminPage = ({
           <h2 className='text-2xl font-semibold mb-4'>쿠폰 관리</h2>
           <div className='bg-white p-4 rounded shadow'>
             <AdminNewCoupon onCouponAdd={onCouponAdd} />
-            <div>
-              <h3 className='text-lg font-semibold mb-2'>현재 쿠폰 목록</h3>
-              <div className='space-y-2'>
-                {couponList.map((coupon, index) => (
-                  <div
-                    key={index}
-                    data-testid={`coupon-${index + 1}`}
-                    className='bg-gray-100 p-2 rounded'
-                  >
-                    {coupon.name} ({coupon.code}):
-                    {coupon.discountType === 'amount'
-                      ? `${coupon.discountValue}원`
-                      : `${coupon.discountValue}%`}{' '}
-                    할인
-                  </div>
-                ))}
-              </div>
-            </div>
+            <AdminCouponList couponList={couponList} />
           </div>
         </div>
       </div>
