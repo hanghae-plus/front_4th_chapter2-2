db1137f0b638ed20f7602a5c6e0b3c27b263aceb
confidential-nt <youyoy689@gmail.com>
2025-01-16 01:38:03 +0900

    refactor: 어드민 - 쿠폰 관심사 리팩토링

---
+++ src/refactoring/hooks/useNewCoupon.ts
@@ -0,0 +1,51 @@
+import { ChangeEvent, useState } from "react";
+import { Coupon } from "../../types";
+
+export const useNewCoupon = () => {
+  const [newCoupon, setNewCoupon] = useState<Coupon>({
+    name: "",
+    code: "",
+    discountType: "percentage",
+    discountValue: 0,
+  });
+
+  const setNewCouponName = (name: string) => {
+    setNewCoupon({ ...newCoupon, name });
+  };
+
+  const setNewCouponCode = (code: string) => {
+    setNewCoupon({ ...newCoupon, code });
+  };
+
+  const setNewCouponDiscountType = (discountType: "amount" | "percentage") => {
+    setNewCoupon({
+      ...newCoupon,
+      discountType,
+    });
+  };
+
+  const setNewCouponDiscountValue = (discountValue: number) => {
+    setNewCoupon({
+      ...newCoupon,
+      discountValue,
+    });
+  };
+
+  const resetNewCoupon = () => {
+    setNewCoupon({
+      name: "",
+      code: "",
+      discountType: "percentage",
+      discountValue: 0,
+    });
+  };
+
+  return {
+    newCoupon,
+    setNewCouponName,
+    setNewCouponCode,
+    setNewCouponDiscountType,
+    setNewCouponDiscountValue,
+    resetNewCoupon,
+  };
+};
