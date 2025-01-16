f3ea9a2a0e963c24e224baf4c8e9e107447f11a2
nkcfe <chul.develop@gmail.com>
2025-01-17 04:27:01 +0900

    Feat: pr template 작성

--- src/refactoring/pages/AdminPage.tsx
+++ src/refactoring/pages/AdminPage.tsx
@@ -23,6 +23,7 @@ export const AdminPage = (props: Props) => {
     onCouponAdd,
     onGradeAdd,
   } = props;
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">관리자 페이지</h1>
