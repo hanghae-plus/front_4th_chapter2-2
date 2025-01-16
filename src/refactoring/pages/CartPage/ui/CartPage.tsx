958c6e924701169863485c667759d1202b6ee3b7
effozen <ashgrayblue0@gmail.com>
2025-01-16 23:30:46 +0900

    feat: SelectedItem 분리

--- src/refactoring/pages/CartPage/ui/CartPage.tsx
+++ src/refactoring/pages/CartPage/ui/CartPage.tsx
@@ -1,8 +1,7 @@
 import { Coupon, Product } from '@/shared/types/';
 import { useCartStore } from '@/entities/cart';
-import { getAppliedDiscount } from '@/entities/cart';
 import { CartItem, GridContainer, GridItem } from '@/widgets/CartItem';
-import { Button } from '@/shared/ui';
+import { SelectedItem } from '@/widgets/SelectedItem';
 
 interface Props {
   products: Product[];
@@ -10,7 +9,7 @@ interface Props {
 }
 
 export function CartPage({ products, coupons }: Props) {
-  const { cart, removeFromCart, updateQuantity, applyCoupon, calculateTotal, selectedCoupon } = useCartStore();
+  const { cart, applyCoupon, calculateTotal, selectedCoupon } = useCartStore();
 
   const { totalBeforeDiscount, totalAfterDiscount, totalDiscount } = calculateTotal();
 
@@ -28,32 +27,7 @@ export function CartPage({ products, coupons }: Props) {
       <GridItem title={'장바구니 내역'}>
         <div className="space-y-2">
           {cart.map((item) => {
-            const appliedDiscount = getAppliedDiscount(item);
-            return (
-              <div key={item.product.id} className="flex justify-between items-center bg-white p-3 rounded shadow">
-                <div>
-                  <span className="font-semibold">{item.product.name}</span>
-                  <br />
-                  <span className="text-sm text-gray-600">
-                    {item.product.price}원 x {item.quantity}
-                    {appliedDiscount > 0 && (
-                      <span className="text-green-600 ml-1">({(appliedDiscount * 100).toFixed(0)}% 할인 적용)</span>
-                    )}
-                  </span>
-                </div>
-                <div>
-                  <Button variant="primary" onClick={() => updateQuantity(item.product.id, item.quantity - 1)}>
-                    -
-                  </Button>
-                  <Button variant="primary" onClick={() => updateQuantity(item.product.id, item.quantity + 1)}>
-                    +
-                  </Button>
-                  <Button variant="danger" onClick={() => removeFromCart(item.product.id)}>
-                    삭제
-                  </Button>
-                </div>
-              </div>
-            );
+            return <SelectedItem item={item} />;
           })}
         </div>
 
