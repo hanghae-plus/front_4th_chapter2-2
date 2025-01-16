56f25e26548d4cb0b866dd76d07b0f9a3798e99c
jch1223 <ico1828@gmail.com>
2025-01-17 01:03:21 +0900

    ♻️ UseCouponForm hook으로 로직 분리

--- src/refactoring/hooks/useForm.ts
+++ src/refactoring/hooks/useForm.ts
@@ -3,9 +3,13 @@ import { useState } from 'react';
 export const useForm = <T>(initialState: T) => {
   const [value, setValue] = useState<T>(initialState);
 
+  const init = () => {
+    setValue(initialState);
+  };
+
   const updateValue = (key: keyof T, value: T[keyof T]) => {
     setValue(prev => ({ ...prev, [key]: value }));
   };
 
-  return { value, updateValue };
+  return { value, updateValue, init };
 };
--- src/refactoring/pages/Admin/CouponManagement/components/CouponAddForm/index.tsx
+++ src/refactoring/pages/Admin/CouponManagement/components/CouponAddForm/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import { useCouponForm } from '@/refactoring/pages/Admin/CouponManagement/components/CouponAddForm/hooks/useCouponForm';
 import type { Coupon } from '@/types';
 
 interface CouponAddFormProps {
@@ -7,21 +6,11 @@ interface CouponAddFormProps {
 }
 
 export const CouponAddForm = ({ onCouponAdd }: CouponAddFormProps) => {
-  const [newCoupon, setNewCoupon] = useState<Coupon>({
-    name: '',
-    code: '',
-    discountType: 'percentage',
-    discountValue: 0
-  });
+  const { editingCoupon, updateName, updateCode, updateDiscountType, updateDiscountValue, init } = useCouponForm();
 
   const handleAddCoupon = () => {
-    onCouponAdd(newCoupon);
-    setNewCoupon({
-      name: '',
-      code: '',
-      discountType: 'percentage',
-      discountValue: 0
-    });
+    onCouponAdd(editingCoupon);
+    init();
   };
 
   return (
@@ -29,22 +18,22 @@ export const CouponAddForm = ({ onCouponAdd }: CouponAddFormProps) => {
       <input
         type="text"
         placeholder="쿠폰 이름"
-        value={newCoupon.name}
-        onChange={e => setNewCoupon({ ...newCoupon, name: e.target.value })}
+        value={editingCoupon.name}
+        onChange={e => updateName(e.target.value)}
         className="w-full rounded border p-2"
       />
       <input
         type="text"
         placeholder="쿠폰 코드"
-        value={newCoupon.code}
-        onChange={e => setNewCoupon({ ...newCoupon, code: e.target.value })}
+        value={editingCoupon.code}
+        onChange={e => updateCode(e.target.value)}
         className="w-full rounded border p-2"
       />
 
       <div className="flex gap-2">
         <select
-          value={newCoupon.discountType}
-          onChange={e => setNewCoupon({ ...newCoupon, discountType: e.target.value as 'amount' | 'percentage' })}
+          value={editingCoupon.discountType}
+          onChange={e => updateDiscountType(e.target.value as 'amount' | 'percentage')}
           className="w-full rounded border p-2"
         >
           <option value="amount">금액(원)</option>
@@ -53,8 +42,8 @@ export const CouponAddForm = ({ onCouponAdd }: CouponAddFormProps) => {
         <input
           type="number"
           placeholder="할인 값"
-          value={newCoupon.discountValue}
-          onChange={e => setNewCoupon({ ...newCoupon, discountValue: parseInt(e.target.value) })}
+          value={editingCoupon.discountValue}
+          onChange={e => updateDiscountValue(parseInt(e.target.value))}
           className="w-full rounded border p-2"
         />
       </div>
