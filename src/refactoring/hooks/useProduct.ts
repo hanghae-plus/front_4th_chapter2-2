a951a2b82392ac467274d0fb121e1f7d535ffa4c
haaneul11 <sksmsqodn20@naver.com>
2025-01-16 21:40:05 +0900

    refactor : useProduct hook 수정

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -3,17 +3,18 @@ import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
   // 특정 제품으로 초기화 할 수 있다.
-  cosnt[(products, setProducts)] = useState<Product[]>(initialProducts);
+  const [products, setProducts] = useState<Product[]>(initialProducts);
 
   // 제품을 업데이트 할 수 있다.
-  const updateProduct = (productId: string, updateProduct: Product) => {
-    setProducts((prevProducts) => {
-      return prevProducts.map((product) => {
-        if (product.id === productId) {
-          return { ...product, ...updateProduct };
-        }
-      });
+  const updateProduct = (updateProduct) => {
+    const newProductList = products.map((prevProduct) => {
+      if (prevProduct.id === updateProduct.id) {
+        return updateProduct;
+      } else {
+        return prevProduct;
+      }
     });
+    setProducts(newProductList);
   };
 
   // 새로운 제품을 추가할 수 있다.
