89a7b538081dc1b81aa4dc7c69eab2c834321fb3
DESKTOP-PPQ6A24\USER <thddl2079@naver.com>
2025-01-16 23:05:54 +0900

    refactor: Button 컴포넌트 적용

--- src/refactoring/components/common/Button.tsx
+++ src/refactoring/components/common/Button.tsx
@@ -19,11 +19,18 @@ const Button = ({
     {
       fullBlueBtn:
         "w-full bg-blue-500 text-white hover:bg-blue-600  px-3 py-1 rounded",
+
       fullGrayBtn:
         "w-full bg-gray-300 text-gray-500 cursor-not-allowed px-3 py-1 rounded",
       smallGrayBtn:
         "bg-gray-300 text-gray-800 px-2 py-1 rounded mr-1 hover:bg-gray-400",
+
       smallRedBtn: "bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600",
+
+      smallGreenBtn:
+        "bg-green-500 text-white px-4 py-2 rounded mb-4 hover:bg-green-600",
+      fullGreenBtn:
+        "w-full bg-green-500 text-white p-2 rounded hover:bg-green-600",
     }[disabled ? "disabled" : variant] || "";
 
   return (
