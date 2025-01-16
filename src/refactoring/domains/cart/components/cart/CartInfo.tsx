3faaed27728239a690c37a3155219b17a1ea3e57
Changyeop Song <waitwait0301@naver.com>
2025-01-16 17:09:36 +0900

    refactor: CartInfo 내부의 getAppliedDiscount를 같은 도메인 폴더 내부 models로 분리

--- src/refactoring/domains/cart/components/cart/CartInfo.tsx
+++ src/refactoring/domains/cart/components/cart/CartInfo.tsx
@@ -1,3 +1,5 @@
+import { getAppliedDiscount } from '../models/getAppliedDiscount';
+
 import type { CartItem } from '../../../../../types';
 
 interface CartInfoProps {
@@ -5,19 +7,6 @@ interface CartInfoProps {
 }
 
 export const CartInfo = ({ item }: CartInfoProps) => {
-  const getAppliedDiscount = (item: CartItem) => {
-    const { discounts } = item.product;
-    const { quantity } = item;
-
-    let appliedDiscount = 0;
-    for (const discount of discounts) {
-      if (quantity >= discount.quantity) {
-        appliedDiscount = Math.max(appliedDiscount, discount.rate);
-      }
-    }
-    return appliedDiscount;
-  };
-
   const appliedDiscount = getAppliedDiscount(item);
 
   return (
